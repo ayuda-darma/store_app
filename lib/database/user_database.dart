4d00dae870a2405c9814dfbceef6c020bba11ce2
Ayuda Darmawan Yulianto <ayuda.darmawan@gmail.com>
2022-06-10 23:27:07 +0700

    update db

--- lib/database/user_database.dart
+++ lib/database/user_database.dart
@@ -45,7 +45,7 @@ class DatabaseProvider {
     await database.execute("CREATE TABLE $userTable ("
         "id INTEGER PRIMARY KEY, "
         "username TEXT, "
-        "password TEXT "
+        "token TEXT "
         ")");
   }
 }
